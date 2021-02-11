93d74ded37b21f6a035a81b3a01edce25e884cfd
Kato <kubkate@gmail.com>
2021-02-10 16:49:48 -0800

    set up pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import RoomInfo from './pages/RoomInfo';
+import Error from './pages/Error';
 import './App.css';
 
 const App = props => {
   return (
-    <div>
-      hello from App
-    </div>
+    <React.Fragment>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={RoomInfo} />
+        <Route component={Error} />
+      </Switch> 
+    </React.Fragment>
   )
 }
 
---
+++ src/pages/RoomInfo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class RoomInfo extends Component {
+  render() {
+    return (
+      <div>
+        this is single room page
+      </div>
+    )
+  }
+}
