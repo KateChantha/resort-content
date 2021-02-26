bd36e8b2a022009f6da64e9096dad4c51fd7cd0b
Kato <kubkate@gmail.com>
2021-02-25 17:45:17 -0800

    basic set up RoomsContainer

---
+++ src/components/RoomsContainer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import RoomFilter from './RoomFilter';
+import RoomList from './RoomList';
+
+
+const RoomsContainer = () => {
+  return (
+    <div>
+      this is room container
+      <RoomFilter />
+      <RoomList />
+    </div>
+  )
+}
+
+export default RoomsContainer
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -2,16 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Hero from '../components/Hero';
 import Banner from '../components/Banner';
+import RoomsContainer from '../components/RoomsContainer';
 
 const Rooms = () => {
   return (
-    <Hero heroStyle="roomsHero">
-      <Banner title="our rooms">
-        <Link to="/" className="btn-primary">
-          return home
-        </Link>
-      </Banner>
-    </Hero>
+    <React.Fragment>
+      <Hero heroStyle="roomsHero">
+        <Banner title="our rooms">
+          <Link to="/" className="btn-primary">
+            return home
+          </Link>
+        </Banner>
+      </Hero>
+      <RoomsContainer />
+    </React.Fragment>
+    
   )
 }
 
