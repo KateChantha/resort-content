e4eaa3aa58839378885bc861492ab00874d61e80
Kato <kubkate@gmail.com>
2021-03-09 12:54:14 -0800

    clean up code

--- src/components/AboutYourStay.js
+++ src/components/AboutYourStay.js
@@ -7,7 +7,6 @@ import './AboutYourStay.css';
 
 const AboutYourStay = () => {
   const context = useContext(RoomContext);
-  // const { capacity, dateRange } = context;
   const { capacity, dateRange, pickedRoom } = context;
   
   const formatDate = (date) => {
@@ -16,8 +15,8 @@ const AboutYourStay = () => {
     else return dateArray[0]+","+dateArray[1]+" "+dateArray[2]+","+dateArray[3]
   }
 
-  console.log("in AboutYourStay.js context",context )
   const priceTimeNights = pickedRoom.price * dateRange.nightStay;
+
   return (
     <div className="about-your-stay">
       <Title title="about your stay" />
@@ -37,16 +36,9 @@ const AboutYourStay = () => {
       <p> {dateRange.nightStay}{dateRange.nightStay > 1 ? " Nights": " Night" }</p>
       <div>
         { pickedRoom.roomName && <span>{pickedRoom.roomName}</span>}
-        { pickedRoom.price && <span> ${priceTimeNights}</span>}
+        { pickedRoom.price && <span> ${priceTimeNights}.00</span>}
       </div>
-      {/* <div>
-        <span>Taxes and Fees</span>
-        { pickedRoom.price 
-          ? <span> ${(pickedRoom.price * dateRange.nightStay)*0.125 }</span>
-          : <span>$0.00</span>
-        }
-      </div> */}
-      
+
         { pickedRoom.price && (
           <div>
             <span>Taxes and Fees</span>
