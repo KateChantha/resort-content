377a9857db0b0dad386e27c4b4cf68ae858c5d3b
Kato <kubkate@gmail.com>
2021-03-09 17:57:32 -0800

    finish ocerall stlying

---
+++ src/components/SubTitle.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SubTitle = ({ title }) => {
+  return (
+    <div className="section-subtitle">
+      <h6>{title}</h6>
+      <div />
+    </div>
+  )
+}
+
+export default SubTitle
